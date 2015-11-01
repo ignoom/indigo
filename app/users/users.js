9043685afd62804d85db843cb641fecdbdd5f7b2
Oswaldo Avalos <oavalos@agari.com>
2015-10-31 23:37:20 -0700

    Fixed routes
    -Si

--- app/users/users.js
+++ app/users/users.js
@@ -4,65 +4,66 @@ angular.module('indigo.users', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-  		when('/login', {
-    		templateUrl: 'users/login.html',
-    		controller: 'UsersCtrl'
+  when('/login', {
+    templateUrl: 'users/login.html',
+    controller: 'UsersCtrl'
   }).
-  		when('/uregister', {
-    		templateUrl: 'users/register.html',
-   			controller: 'UsersCtrl'
+  when('/uregister', {
+    templateUrl: 'users/register.html',
+    controller: 'UsersCtrl'
   });
 }])
 
 .controller('UsersCtrl', ['$scope', '$http', function($scope, $http) {
 
-  $scope.register = function(user){
-    console.log("Registering user", user );
+  $scope.register = function(user) {
+    console.log("Registering user", user);
     // TODO while trying to register a client show a spinner or something in here
-    $http.post('http://localhost:1337/users/users', user)
+    $http.post('http://localhost:1337/users/local/register', user)
       .success($scope.registerSuccess)
       .error($scope.registerError);
   };
 
-  $scope.registerSuccess = function(data, status, headers, config){
-    console.log("Data",data);
+  $scope.registerSuccess = function(data, status, headers, config) {
+    console.log("Data", data);
     console.log("Status", status);
     $scope.resetForm($scope.usersRegister);
   };
 
-  $scope.registerError = function(data, status, headers, config){
+  $scope.registerError = function(data, status, headers, config) {
     console.log("error", data);
     console.log("status", status);
     // TODO display some kind of error in here
   };
-//I added the following lines for the login... not sure if needed
-    $scope.login = function(user){
-    console.log("Login user", user );
+  //I added the following lines for the login... not sure if needed
+  $scope.login = function(user) {
+    console.log("Login user", user);
+    user.client_id = '5o6NHxGe8uTKF2EXzhQf9p6lDxPUNd1L';
+    user.grant_type = 'password';
     // TODO while trying to register a client show a spinner or something in here
-    $http.post('http://localhost:1337/login', user)
-      .success($scope.registerSuccess)
-      .error($scope.registerError);
+    $http.post('http://localhost:1337/oauth/token', user)
+      .success($scope.loginSuccess)
+      .error($scope.loginError);
   };
 
-  $scope.loginSuccess = function(data, status, headers, config){
-    console.log("Data",data);
+  $scope.loginSuccess = function(data, status, headers, config) {
+    console.log("Data", data);
     console.log("Status", status);
     $scope.resetForm($scope.usersLogin);
   };
 
-  $scope.loginError = function(data, status, headers, config){
+  $scope.loginError = function(data, status, headers, config) {
     console.log("error", data);
     console.log("status", status);
     // TODO display some kind of error in here
   };
 
-  $scope.resetForm = function(form){
-    if(form){
+  $scope.resetForm = function(form) {
+    if (form) {
       form.$setPristine();
       form.$setUntouched();
     }
-    $scope.client = {};
+    $scope.user = {};
   }
 
 }]);
-
