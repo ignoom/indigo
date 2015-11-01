46725d804bbfe2f91742511c68b0ea2baa548ec3
Oswaldo Avalos <oavalos@agari.com>
2015-10-31 19:32:18 -0700

    Form submits a post request to backend
    -Added controller side logic to handle submissions on forms
    -TODO: Need to show error messages, and success banners

--- app/clients/clients.js
+++ app/clients/clients.js
@@ -1,13 +1,39 @@
 'use strict';
 
 angular.module('indigo.clients', ['ngRoute'])
-
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/register', {
     templateUrl: 'clients/register.html',
     controller: 'clientsCtrl'
   });
 }])
-.controller('clientsCtrl', [function() {
+.controller('clientsCtrl', ['$scope', '$http', function($scope, $http) {
 
+  $scope.register = function(client){
+    console.log("Registering client", client );
+    // TODO while trying to register a client show a spinner or something in here
+    $http.post('http://localhost:1337/clients/local/register', client)
+      .success($scope.registerSuccess)
+      .error($scope.registerError);
+  };
+
+  $scope.registerSuccess = function(data, status, headers, config){
+    console.log("Data",data);
+    console.log("Status", status);
+    $scope.resetForm($scope.clientsRegister);
+  };
+
+  $scope.registerError = function(data, status, headers, config){
+    console.log("error", data);
+    console.log("status", status);
+    // TODO display some kind of error in here
+  };
+
+  $scope.resetForm = function(form){
+    if(form){
+      form.$setPristine();
+      form.$setUntouched();
+    }
+    $scope.client = {};
+  }
 }]);
