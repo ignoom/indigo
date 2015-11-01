2ee6bee0913a3d0eefca8d43bc56876ca1aaf1c0
Jannine Torres <jannine.gc@hotmail.com>
2015-10-31 23:59:39 -0600

    Post request to backend for users
    required fields in login form are not working properly.

--- app/users/users.js
+++ app/users/users.js
@@ -14,6 +14,55 @@ angular.module('indigo.users', ['ngRoute'])
   });
 }])
 
-.controller('UsersCtrl', [function() {
+.controller('UsersCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.register = function(user){
+    console.log("Registering user", user );
+    // TODO while trying to register a client show a spinner or something in here
+    $http.post('http://localhost:1337/users/users', user)
+      .success($scope.registerSuccess)
+      .error($scope.registerError);
+  };
+
+  $scope.registerSuccess = function(data, status, headers, config){
+    console.log("Data",data);
+    console.log("Status", status);
+    $scope.resetForm($scope.usersRegister);
+  };
+
+  $scope.registerError = function(data, status, headers, config){
+    console.log("error", data);
+    console.log("status", status);
+    // TODO display some kind of error in here
+  };
+//I added the following lines for the login... not sure if needed
+    $scope.login = function(user){
+    console.log("Login user", user );
+    // TODO while trying to register a client show a spinner or something in here
+    $http.post('http://localhost:1337/login', user)
+      .success($scope.registerSuccess)
+      .error($scope.registerError);
+  };
+
+  $scope.loginSuccess = function(data, status, headers, config){
+    console.log("Data",data);
+    console.log("Status", status);
+    $scope.resetForm($scope.usersLogin);
+  };
+
+  $scope.loginError = function(data, status, headers, config){
+    console.log("error", data);
+    console.log("status", status);
+    // TODO display some kind of error in here
+  };
+
+  $scope.resetForm = function(form){
+    if(form){
+      form.$setPristine();
+      form.$setUntouched();
+    }
+    $scope.client = {};
+  }
 
 }]);
+
