5a094c7fe0f25c460640832bc66fd1c8fbe36672
Oswaldo Avalos <oavalos@agari.com>
2015-11-01 00:22:24 -0700

    Added succcess and error messages to login form

--- app/users/users.js
+++ app/users/users.js
@@ -14,7 +14,7 @@ angular.module('indigo.users', ['ngRoute'])
   });
 }])
 
-.controller('UsersCtrl', ['$scope', '$http', function($scope, $http) {
+.controller('UsersCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 
   $scope.register = function(user) {
     console.log("Registering user", user);
@@ -50,13 +50,32 @@ angular.module('indigo.users', ['ngRoute'])
     console.log("Data", data);
     console.log("Status", status);
     $scope.resetForm($scope.usersLogin);
+    // Display success
+    var success_obj = {data: "Login success "+data.access_token, status: status};
+    $scope.showMessage(success_obj);
+    // TODO save the session bearer token
   };
 
   $scope.loginError = function(data, status, headers, config) {
     console.log("error", data);
     console.log("status", status);
+    // Display message
+    var error_obj = {data: data.error_description, status: status};
+    $scope.showMessage(error_obj);
     // TODO display some kind of error in here
   };
+
+  $scope.showMessage = function(obj){
+    $scope.formMessage = obj.data;
+    // TODO set up a timeout to hide it
+    if(obj.status == 200){
+      $timeout($scope.hideMessage, 3000);
+    }
+  }
+
+  $scope.hideMessage = function(){
+    $scope.formMessage = null;
+  }
 
   $scope.resetForm = function(form) {
     if (form) {
